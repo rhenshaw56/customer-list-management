fc58e3f4bfa2bc4d408ac157c5c0490a288083d3
rowland henshaw <rowland.henshaw@andela.com>
2019-05-27 12:50:49 +0100

    setup routing and bootstrapped componenets

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, withRouter, RouteComponentProps, Link }  from 'react-router-dom';
+import Home from './components/Home';
+import Create from './components/customer/Create'
+import Edit from './components/customer/Edit';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component<RouteComponentProps<any>> {
+  public render() {
+    return (
+      <div>
+        <nav>
+          <ul><li><Link to={'/'}>Home</Link></li></ul>
+          <ul><li><Link to={'/create'}>Create Customer</Link></li></ul>
+        </nav>
+        <Switch>
+          <Route path={'/'} exact component={Home} />
+          <Route path={'/create'} exact component={Create} />
+          <Route path={'/edit/:id'} exact component={Edit} />
+        </Switch>
+      </div>
+    )
+  }
+
 }
 
-export default App;
+export default withRouter(App);
